abf8fe0138a0fcd942347ee3239fcc3a1ca32c87
Farzad Mohtasham <farzaddddmohtasham@gmail.com>
2025-07-29 16:07:30 +0330

    Added env

--- cmd/api/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/FarzadMohtasham/go-social
-
-go 1.24.4
-
-require (
-	github.com/go-chi/chi v1.5.5
-	github.com/go-chi/chi/v5 v5.2.2
-)
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/FarzadMohtasham/go-social/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8880",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
